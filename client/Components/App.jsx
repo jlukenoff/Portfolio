5250e7b61a4264b9fbdcdb56049295ed61b7a0d6
John Lukenoff <jlukenoff@gmail.com>
2018-09-14 14:14:24 -0700

    Add background to landing page

---
+++ client/Components/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...props,
+    };
+  }
+
+  render() {
+    return (
+      <div className={styles.rootComponent}>hello from react</div>
+    );
+  }
+}
+
+App.propTypes = {
+};
+
+export default App;
