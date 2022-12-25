f3c77f8d340198e04f689bafb2a2309d88fb52e5
John Lukenoff <jlukenoff@gmail.com>
2022-12-25 08:22:31 -0800

    Add a three js box

--- apps/main/pages/about.tsx
+++ apps/main/pages/about.tsx
@@ -1,18 +1,47 @@
-import { Box } from "@mui/material";
+import React, { useRef, useEffect, useState } from "react";
 import { NextPage } from "next";
+import * as THREE from "three";
+import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
+
+interface BoxProps {
+  position: [x: number, y: number, z: number];
+}
+
+const Box: React.FC<BoxProps> = (props) => {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef<THREE.Mesh>(null);
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) =>
+    ref.current ? (ref.current.rotation.x += 0.01) : 0
+  );
+
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+};
 
 const About: NextPage = () => {
   return (
-    <Box
-      sx={{
-        height: "20rem",
-        width: "100%",
-        maxWidth: "50rem",
-        border: "1px solid red",
-      }}
-    >
-      Content Here
-    </Box>
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
   );
 };
 
